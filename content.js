c065553af312febe2eff31d23b1e59d518b4d500
Luiz Fellype <lfcassago@gmail.com>
2022-10-06 11:12:38 -0300

    feat: add timeframes shortcut
    refactor code into functions

--- content.js
+++ content.js
@@ -1,55 +1,131 @@
-const buttonClassNames = ["button", "button--small", "header__sidebar-button"]
+const drawsButtonClassNames = ["button", "button--small", "header__sidebar-button"]
 
-const createButton = (content = '', positionOnList) => {
+const getDrawersList = () => {
+  const settingsBtn = document.querySelectorAll('.trading-chart-settings__item')[0]
+  settingsBtn.click();
+  const menuList = document.querySelectorAll('.sidepanel__menu-item')
+
+  return menuList
+}
+
+const closeMenuList = () => {
+  const settingsBtn = document.querySelectorAll('.trading-chart-settings__item')[0]
+  settingsBtn.click();
+  const menuList = document.querySelectorAll('.sidepanel__menu-item')
+
+  return menuList
+}
+
+const createButton = (getMenuList, shouldCloseMenu = true, buttonClassNames = drawsButtonClassNames) => (content = '', positionOnList, onclick = () => { }) => {
   const button = document.createElement("button")
-  
+
   buttonClassNames.forEach(className => button.classList.add(className))
   button.innerHTML = content;
-  console.log('QX >>>', {button});
-   button.onclick = positionOnList !== undefined ? async () => {
-    const settingsBtn = document.querySelectorAll('.trading-chart-settings__item')[0]
-    settingsBtn.click();    
-    const menuList = await document.querySelectorAll('.sidepanel__menu-item')
-    menuList[positionOnList].click();    
-    document.querySelectorAll('.sidepanel__close')[0].click();
-  } : () => {
+  button.onclick = positionOnList !== undefined ? () => {
+    const menuList = getMenuList()
+    menuList[positionOnList].click();
+    shouldCloseMenu && closeMenuList()
+  } : onclick
 
-  }
-  
   return button
 }
 
 const buttonsWrapperClass = "shortcut-wrapper"
+const createDrawsShortcutOnHeader = () => {
+  const buttonsWrapper = document.createElement("div")
+  buttonsWrapper.classList.add(buttonsWrapperClass)
 
-let interval = setInterval(() => {
+  // const configWrapper =  document.createElement("div")
+  // const allItens = document.querySelectorAll(".sidepanel__menu-item")
+  // [...allItens].map(e => {
+  // button = document.createElement("button")
+  // button.innerHTML = e.innerHTML
+  // configWrapper.appendChild(ib)    })
+  // const configWrapper =  document.createElement("div")
+  // const a = () => {
+  //   const menuList = getMenuList()
+  // }
+
+  const createButtons = buttonData => {
+    const button = createButton(getDrawersList)(...buttonData)
+    buttonsWrapper.appendChild(button)
+  }
+
+  [['Trend', 9], ['Horizontal', 11], ['Fibo', 19]].forEach(createButtons)
+
+  return buttonsWrapper
+}
+
+const timeframeWrapperClassnames = 'timeframe-wrapper'
+const timeframeButtonClassNames = ['trading-chart-settings__item']
+
+const getTimeframeList = () => {
+  const currentTimeFrameButton = document.getElementsByClassName('trading-chart-settings__item')[1]
+  currentTimeFrameButton.click()
+  const timeframeButtons = document.getElementsByClassName('popover-select__settings-time-item')
+
+  return timeframeButtons
+}
+
+const createTimeframShortcut = () => {
+  const timeframeWrapper = document.createElement("div")
+  timeframeWrapper.classList.add(timeframeWrapperClassnames)
+
+  const buttons = [['1m', 4], ['5m', 7], ['15m', 9]]
+
+  const createButtons = buttonData => {
+    const shouldCloseMenu = false
+    const button = createButton(getTimeframeList, shouldCloseMenu, timeframeButtonClassNames)(...buttonData)
+    timeframeWrapper.appendChild(button)
+  }
+
+  buttons.forEach(createButtons)
+
+  return timeframeWrapper
+}
+
+const increaseLiveTiming = () => {
+  let findServerLiveTimeInterval = setInterval(() => {
+    const onGoingTime = document.getElementsByClassName('server-time online')[0]
+
+    if (onGoingTime) {
+      clearInterval(findServerLiveTimeInterval)
+      onGoingTime.style.fontSize = '20px';
+    }
+  }, 1000)
+
+}
+
+const setTimeFrameShortcutsIntoSettingsWrapper = (timeframeButtonsWrapper) => {
+  let findTrandingSettingsInterval = setInterval(() => {
+    const rightSidebar = document.getElementsByClassName('sidebar-section')[0]
+
+    if (rightSidebar) {
+      clearInterval(findTrandingSettingsInterval)
+      rightSidebar.appendChild(timeframeButtonsWrapper)
+    }
+  }, 1000)
+}
+
+
+// main execution
+let findMenuInterval = setInterval(() => {
   let menu = document.querySelector(".header__container")
 
   if (menu) {
     try {
-      clearInterval(interval)
-      const onGoingTime = document.getElementsByClassName('server-time online')[0]
-      onGoingTime && (onGoingTime.style.fontSize = '20px');
+      clearInterval(findMenuInterval)
 
-      const buttonsWrapper = document.createElement("div")
-      buttonsWrapper.classList.add(buttonsWrapperClass)
-      
-      const setupButtonsOnDivWrapper = buttonData => {
-        const button = createButton(...buttonData)
-        buttonsWrapper.appendChild(button)
-      }
-      
-      [['Trend', 9], ['Horizontal', 11], ['Fibo', 19]].forEach(setupButtonsOnDivWrapper)
-      // const configWrapper =  document.createElement("div")
-      // const allItens = document.querySelectorAll(".sidepanel__menu-item")
-      // [...allItens].map(e => {
-      // button = document.createElement("button")
-      // button.innerHTML = e.innerHTML
-      // configWrapper.appendChild(ib)    })
-      
-      menu.appendChild(buttonsWrapper)
+      increaseLiveTiming()
+
+      const drawButtonsWrapper = createDrawsShortcutOnHeader()
+      menu.appendChild(drawButtonsWrapper)
+
+      const timeframeButtonsWrapper = createTimeframShortcut()
+      setTimeFrameShortcutsIntoSettingsWrapper(timeframeButtonsWrapper)
+     
     } catch (error) {
-      console.log('QX >>> error', {error})
-      
+      console.log('QX >>> error', { error })
     }
   }
 }, 1000);
